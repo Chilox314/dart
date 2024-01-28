2d79cd456b2415b5a32db6ee283c67404a096767
ChristianMac <syncforcoding@gmail.com>
2024-01-28 20:43:45 +0100

    restructured everything, added todo

---
+++ gui/to-do.md
@@ -0,0 +1,6 @@
+# To Do List GUI
+* document everything
+* change the coords from static values to dynamic ones that get calculated from the window size
+* Add the big values around the board
+* make output, gamemode chooser etc.
+* wire everything up with the existing game modes
---
+++ working_projects/dart.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#IMPORT FEHLER IST NICHT REAL!!! ARGUMENTE BEIM START NÖTIG!!!
+
+#Möglichkeit einer Session mit Sessionstats und -vergleich (Alias 3x hintereinander spielen und dann den Direktvergleich haben)
+#Option zum plotten adden
+#GUI bauen (TKinter?)
+
+from working_projects.atc import play_around_the_clock
+from working_projects.triples import play_triples
+import sys, argparse
+
+def main():
+    args = paste_command_line(sys.argv[1:])
+    if args.plot:
+        print('plot')
+        plot_scores_txt()
+    elif args.aroundTheClock:
+        play_around_the_clock()
+    elif args.triples:
+        play_triples(input('What triple do you like to focus on?\n'))
+    else:
+        print('Please use Arguments. See ./dart.py -h')
+
+#here is the code for plotting
+def plot_scores_txt():
+    return True
+
+def paste_command_line(argv):
+    p = argparse.ArgumentParser(description='This is for playing "Around the clock" or other dart game modes. Have Fun!')
+
+    p.add_argument('--plot','-p',action='store_true')
+    p.add_argument('--aroundTheClock','-a',action='store_true',help='Play a game of around the clock')
+    p.add_argument('--triples','-t',action='store_true',help='Play a game of throwing triples')
+
+    return p.parse_args(argv)
+
+main()
